04b8f447bf9b52cb33220de15a24a687c515779a
游城棣 <70698299+phoenix58-justin@users.noreply.github.com>
2020-09-29 21:40:15 +0800

    Update 29020 755200 游城棣 三的倍數.cpp
    已經修改過， 再次感謝!

--- 755200/29020 755200 游城棣 三的倍數.cpp
+++ 755200/29020 755200 游城棣 三的倍數.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 int main(){
     int inputnum;
-    while(cin >> inputnum){
-      if(inputnum % 3 == 0){
+    while (cin >> inputnum) {
+    if (inputnum % 3 == 0) {
         cout << "是三的倍數";
-      }else{
+    } else{
         cout << "非三的倍數";
-      }
+    }
       
     }
 }
+
